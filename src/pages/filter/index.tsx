8426f9d66853d14ee88dbba531de5a4d858af83a
RafDes21 <israeltineo84@gmail.com>
2023-08-08 20:42:08 -0300

    add responsive at filter

--- src/pages/filter/index.tsx
+++ src/pages/filter/index.tsx
@@ -3,9 +3,8 @@ import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../store/hook/hook";
 
-
 import { fetchFilterMovies } from "../../store/thunks/filterThunks";
-import style from "./styles.module.css"
+import styles from "./styles.module.css";
 import { CardFilter } from "../../components";
 
 const Filter = () => {
@@ -18,25 +17,27 @@ const Filter = () => {
     dispatch(fetchFilterMovies(keyword));
   }, [keyword]);
   return (
-    <div className="container-filter">
-      <div className="container-results">
-        <h2>
-          Resultados de tu busqueda:
-          {movies.length === 0 ? "no hay resultados" : keyword}
-        </h2>
-        <div className={style.containerCard}>
-          {movies.length === 0 ? (
-            <div className="sinResults">
-              <h1>no hay nada</h1>
+    <div className={styles.filter}>
+      <h2 className={styles.h2}>
+        Resultados de tu busqueda:
+        {movies.length === 0 ? " no hay resultados" : ` ${keyword}`}
+      </h2>
+      <div className={styles.containerCard}>
+        {movies.length === 0 ? (
+          <div className="sinResults">
+            <h1>no hay nada</h1>
+          </div>
+        ) : (
+          movies.map((movie: any, index) => (
+            <div className={styles.cards}>
+              <CardFilter
+                key={index}
+                title={movie.title}
+                image={movie.poster_path}
+              />
             </div>
-          ) : (
-            movies.map((movie: any, index) => (
-              <div className={style.cards}>
-                <CardFilter key={index} title={movie.title} image={movie.poster_path} />
-              </div>
-            ))
-          )}
-        </div>
+          ))
+        )}
       </div>
     </div>
   );
